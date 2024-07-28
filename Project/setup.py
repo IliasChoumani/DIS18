ab9b577c39faec9b1211275d3561632a8cb9b208
IliasChoumani <92585014+IliasChoumani@users.noreply.github.com>
2024-07-28 13:43:36 +0200

    Add files via upload

---
+++ Project/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_project_tools",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4",
+        "requests"
+    ],
+    entry_points={
+        'console_scripts': [
+            # 'your_script_name = your_module:main_function',
+        ],
+    },
+)
